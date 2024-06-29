80ea643196d737209d568978317d06dbb01a8b25
Abdul Ahad <130489838+AAhad-Dev@users.noreply.github.com>
2024-06-29 14:19:36 +0500

    Fix Dashboard Panel

--- ContextApi/src/Comps/Dashboard.jsx
+++ ContextApi/src/Comps/Dashboard.jsx
@@ -2,16 +2,12 @@ import { useContext } from "react";
 import UserContext from "../context/userContext";
 
 const Dashboard = () => {
-  const { userEmail } = useContext(UserContext);
-
-  if (!userEmail) {
-    return <h1>Plzzz Login</h1>;
-  }
+  const { user } = useContext(UserContext);
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md text-center">
-        <h1 className="text-2xl font-bold mb-4">Welcome, {userEmail.email}!</h1>
+        <h1 className="text-2xl font-bold mb-4">Welcome, {user.email}!</h1>
         <button
           onClick={() => window.history.back()}
           className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
